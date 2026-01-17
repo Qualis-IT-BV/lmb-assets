09170c7343f2b0c2e39872682422f819fb0d3e88
J.P.H. Groeneveld <huibert@qualisit.nl>
2026-01-17 23:13:37 +0100

    Alle JS vanuit Custom CSS en JS plugin omgezet naar GitHub en in Legacy map geplaatst

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,14 +1,22 @@
-
 /* Project: La Maison Bossché
  * Version: 0.0.1
  * Build: dev-20260116.003
- * Source: Custom JS migratie (Wishlist component ausgeloaded)
  */
 
 // Wishlist functionaliteit laden
 (function(){
+	var host = window.location.hostname;
+	var branch;
+	if (host === 'test.lamaisonbossche.nl' || host.indexOf('dev.') === 0) {
+		branch = 'dev';
+	} else if (host === 'staging.lamaisonbossche.nl' || host.indexOf('staging.') === 0) {
+		branch = 'staging';
+	} else {
+		branch = 'main';
+	}
+	
 	var script = document.createElement('script');
-	script.src = 'assets/js/components/Wishlist.js';
+	script.src = 'https://cdn.jsdelivr.net/gh/qualis-it-bv/lmb-assets@' + branch + '/assets/js/components/Wishlist.js';
 	script.async = false;
 	document.head.appendChild(script);
 })();
